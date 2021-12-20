ecbd3c91a0dd1c52eda696d9fbd669708517665a
Sylvain Houssaye <sylvaindu93350@hotmail.fr>
2021-12-20 20:55:10 +0100

    page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,46 @@
+
+Section();
+
+async function getArticles() {
+    const articlesCatch = await fetch ("http://localhost:3000/api/products")
+    return await articlesCatch.json();
+}
+
+async function Section() {
+    let result = await getArticles()
+    .then(function(resultatAPI){
+        const articles = resultatAPI;
+        console.log(articles);
+        for (let article in articles){
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html id=${resultatAPI[article]._id}`;
+
+            // Création de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            // Création de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+            // Création du titre "h3"
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Création de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
+
+        }
+    })
+    .catch (function(error){
+        return error;
+    })
+}
